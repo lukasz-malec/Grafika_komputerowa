0c5b945a9c482da615437584ea2aceaf1f58642a
Łukasz Malec <126986711+lukasz-malec@users.noreply.github.com>
2024-06-10 16:33:35 +0200

    Program z zajęć

--- script.js
+++ script.js
@@ -1,82 +1,3 @@
-
-// Jako argumenty funkcji należy podać lokalizację sześcianu w świecie i zmienną pozwalającą zmienić jego rozmiar.
-
-function generate_box(position, size) {
-    const [x, y, z] = position;
-    const s = size; 
-
-    const vertices = [
-        // Top
-        x - s, y + s, z - s,
-        x - s, y + s, z + s,
-        x + s, y + s, z + s,
-        x + s, y + s, z - s,
-
-        // Left
-        x - s, y + s, z + s,
-        x - s, y - s, z + s,
-        x - s, y - s, z - s,
-        x - s, y + s, z - s,
-
-        // Right
-        x + s, y + s, z + s,
-        x + s, y - s, z + s,
-        x + s, y - s, z - s,
-        x + s, y + s, z - s,
-
-        // Front
-        x + s, y + s, z + s,
-        x + s, y - s, z + s,
-        x - s, y - s, z + s,
-        x - s, y + s, z + s,
-
-        // Back
-        x + s, y + s, z - s,
-        x + s, y - s, z - s,
-        x - s, y - s, z - s,
-        x - s, y + s, z - s,
-
-        // Bottom
-        x - s, y - s, z - s,
-        x - s, y - s, z + s,
-        x + s, y - s, z + s,
-        x + s, y - s, z - s,
-    ];
-
-    const indices = [
-        // Top
-        0, 1, 2,
-        0, 2, 3,
-
-        // Left
-        5, 4, 6,
-        6, 4, 7,
-
-        // Right
-        8, 9, 10,
-        8, 10, 11,
-
-        // Front
-        13, 12, 14,
-        15, 14, 12,
-
-        // Back
-        16, 17, 18,
-        16, 18, 19,
-
-        // Bottom
-        21, 20, 22,
-        22, 20, 23,
-    ];
-
-    return {
-        vertices: new Float32Array(vertices),
-        indices: new Uint16Array(indices)
-    };
-}
-
-
-
 const vertexShaderTxt = `
     precision mediump float;
 
@@ -85,32 +6,56 @@ const vertexShaderTxt = `
     uniform mat4 mProjection;
     
     attribute vec3 vertPosition;
-    attribute vec3 vertColor;
+    attribute vec2 textureCoord;
+    attribute vec3 vertNormal;
 
-    varying vec3 fragColor;
+    varying vec2 fragTextureCoord;
+    varying vec3 fragNormal;
 
     void main() {
-        fragColor = vertColor;
+        fragTextureCoord = textureCoord;
+        fragNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;
+
         gl_Position = mProjection * mView * mWorld * vec4(vertPosition, 1.0);
     }
-`;
-
+`
 const fragmentShaderTxt = `
     precision mediump float;
 
-    varying vec3 fragColor;
+    varying vec2 fragTextureCoord;
+    varying vec3 fragNormal;
+
+    uniform vec3 ambient;
+    uniform vec3 lightDirection;
+    uniform vec3 lightColor;
+
+    uniform sampler2D sampler;
 
     void main() {
-        gl_FragColor = vec4(fragColor, 1.0);
-    }
-`;
+        vec3 normfragNormal = normalize(fragNormal);
+        vec3 normlightDirection = normalize(lightDirection);
 
+
+        vec3 light = ambient  + 
+            lightColor * max(dot(normfragNormal,normlightDirection),0.0);
+
+        vec4 tex = texture2D(sampler, fragTextureCoord);
+
+        gl_FragColor = vec4(tex.rgb * light, tex.a);
+    }
+`
 const mat4 = glMatrix.mat4;
 
-const Triangle = function () {
+function startDraw() {
+    OBJ.downloadMeshes({
+        'maze': 'a-maze.obj'
+    }, Triangle);
+}
+
+const Triangle = function (meshes) {
     const canvas = document.getElementById('main-canvas');
     const gl = canvas.getContext('webgl');
-    let canvasColor = [0.2, 0.5, 0.8];
+    let canvasColor = [0.2, 0.5, 0.8]
 
     checkGl(gl);
 
@@ -119,6 +64,8 @@ const Triangle = function () {
 
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
+
+
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -130,97 +77,85 @@ const Triangle = function () {
     gl.compileShader(fragmentShader);
 
     checkShaderCompile(gl, vertexShader);
-    checkShaderCompile(gl, fragmentShader);
 
     const program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
 
     gl.linkProgram(program);
-    checkLink(gl, program);
 
     gl.detachShader(program, vertexShader);
     gl.detachShader(program, fragmentShader);
 
-    const position1 = [-1, -1, -1]; // lokalizacja pierwszego szescianu 
-    const position2 = [1, 1, 1]; // lokalizacja drugiego sześcianu
-    const size = 0.5; // rozmiar
+    gl.validateProgram(program);
 
+    OBJ.initMeshBuffers(gl, meshes.maze);
 
+    // const boxVertBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, meshes.maze.vertexBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
 
-    const { vertices: vertices1, indices: indices1 } = generate_box(position1, size);
-    const { vertices: vertices2, indices: indices2 } = generate_box(position2, size);
-
-    const vertices = new Float32Array([...vertices1, ...vertices2]);
-    const indices = new Uint16Array([...indices1, ...indices2.map(index => index + vertices1.length / 3)]);
-
-    const boxVertBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, boxVertBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-
-    const boxIndicesBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndicesBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-
+    // const boxIndicesBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshes.maze.indexBuffer);
+    // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
+    
     const posAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     gl.vertexAttribPointer(
         posAttribLocation,
-        3,
+        meshes.maze.vertexBuffer.itemSize,
         gl.FLOAT,
         gl.FALSE,
-        3 * Float32Array.BYTES_PER_ELEMENT,
+        0,
         0
     );
     gl.enableVertexAttribArray(posAttribLocation);
 
-    const colors = [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
 
-        0.8, 0.0, 0.2,
-        0.0, 1.0, 1.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
 
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
-
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
-
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
-
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.2, 1.0,
-        0.5, 1.0, 0.0,
-    ];
-
-    const colorsArray = new Float32Array([...colors, ...colors]);
-
-    const triangleColorBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleColorBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, colorsArray, gl.STATIC_DRAW);
-
-    const colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+    const textureLocation = gl.getAttribLocation(program, 'textureCoord');
     gl.vertexAttribPointer(
-        colorAttribLocation,
-        3,
+        textureLocation,
+        meshes.maze.textureBuffer.itemSize,
         gl.FLOAT,
         gl.FALSE,
-        3 * Float32Array.BYTES_PER_ELEMENT,
+        0,
         0,
     );
-    gl.enableVertexAttribArray(colorAttribLocation);
+    gl.enableVertexAttribArray(textureLocation);
+
+
+    // const boxTextureBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, meshes.maze.normalBuffer);
+    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxTextureCoords), gl.STATIC_DRAW);
+    
+    const normLocation = gl.getAttribLocation(program, 'vertNormal');
+    gl.vertexAttribPointer(
+        normLocation,
+        meshes.maze.textureBuffer.itemSize,
+        gl.FLOAT,
+        gl.TRUE,
+        0,
+        0,
+    );
+    gl.enableVertexAttribArray(normLocation);
+
+
+
+    const img = document.getElementById('img');
+    const boxTexture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texImage2D(
+        gl.TEXTURE_2D,
+        0,
+        gl.RGBA,
+        gl.RGBA,
+        gl.UNSIGNED_BYTE,
+        img
+    )
 
     // render time
 
@@ -231,37 +166,69 @@ const Triangle = function () {
     const projectionMatLoc = gl.getUniformLocation(program, 'mProjection');
 
     const worldMatrix = mat4.create();
+    const worldMatrix2 = mat4.create();
+
     const viewMatrix = mat4.create();
-    mat4.lookAt(viewMatrix, [0, 0, -4], [0, 0, 0], [0, 1, 0]);
+    mat4.lookAt(viewMatrix, [0,0,-12], [0,0,0], [0,1,0]);
     const projectionMatrix = mat4.create();
-    mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(60),
-        canvas.width / canvas.height, 1, 10);
+    mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(60), 
+                    canvas.width/canvas.height, 1, 100)
 
     gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(projectionMatLoc, gl.FALSE, projectionMatrix);
 
+    let ambientLocation = gl.getUniformLocation(program, 'ambient');
+    let lightDirLoc = gl.getUniformLocation(program, 'lightDirection');
+    let lightColorLoc = gl.getUniformLocation(program, 'lightColor');
+
+
+
+    let ambienColor = [0.5, 0.5, 0.2]
+    gl.uniform3f(ambientLocation, ...ambienColor);
+    gl.uniform3f(lightDirLoc, 2.0, 3.0, -1.0);
+    gl.uniform3f(lightColorLoc, 0.4, 0.4, 0.2);
+    
+
+
     const identityMat = mat4.create();
+    let rotationMatrix = new Float32Array(16);
+    let translationMatrix = new Float32Array(16);
     let angle = 0;
 
     const loop = function () {
-        angle = performance.now() / 1000 / 6 * 2 * Math.PI;
-        mat4.rotate(worldMatrix, identityMat, angle, [1, 1, -0.5]);
+        angle = performance.now() / 1000 / 60 * 23 * Math.PI;
+        mat4.rotate(worldMatrix, identityMat, angle, [1,1,-0.5]);
         gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
-
+        
         gl.clearColor(...canvasColor, 1.0);   // R, G, B,  A 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-        gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+        gl.bindTexture(gl.TEXTURE_2D, boxTexture);
+		gl.activeTexture(gl.TEXTURE0); 
+        // gl.drawArrays(gl.TRIANGLES, 0, 24);
+        gl.drawElements(gl.TRIANGLES, meshes.maze.indexBuffer.numItems, 
+                gl.UNSIGNED_SHORT, 0); 
+
+        // rotationMatrix = new Float32Array(16);
+        // translationMatrix = new Float32Array(16);
+
+        // mat4.fromRotation(rotationMatrix, angle/2, [1,2,0]);
+        // mat4.fromTranslation(translationMatrix, [2,0,0]);
+        // mat4.mul(worldMatrix2, translationMatrix, rotationMatrix);
+        // gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix2);
+        // gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
 
         requestAnimationFrame(loop);
-    };
+    }
 
     requestAnimationFrame(loop);
-}
+
+} 
+
 
 function checkGl(gl) {
-    if (!gl) { console.log('WebGL not supported, use another browser'); }
+    if (!gl) {console.log('WebGL not suppoerted, use another browser');}
 }
 
 function checkShaderCompile(gl, shader) {
@@ -275,6 +242,3 @@ function checkLink(gl, program) {
         console.error('linking error', gl.getProgramInfoLog(program));
     }
 }
-
-window.onload = Triangle;
-
