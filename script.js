ca94aeac9838c04d649d8965e2e8de8cbdd53ae3
Łukasz Malec <126986711+lukasz-malec@users.noreply.github.com>
2024-06-10 16:54:10 +0200

    strzałki

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ const vertexShaderTxt = `
     void main() {
         fragTextureCoord = textureCoord;
         fragNormal = (mWorld * vec4(vertNormal, 0.0)).xyz;
-
         gl_Position = mProjection * mView * mWorld * vec4(vertPosition, 1.0);
     }
-`
+`;
+
 const fragmentShaderTxt = `
     precision mediump float;
 
@@ -35,16 +35,22 @@ const fragmentShaderTxt = `
         vec3 normfragNormal = normalize(fragNormal);
         vec3 normlightDirection = normalize(lightDirection);
 
-
-        vec3 light = ambient  + 
+        vec3 light = ambient + 
             lightColor * max(dot(normfragNormal,normlightDirection),0.0);
 
         vec4 tex = texture2D(sampler, fragTextureCoord);
 
         gl_FragColor = vec4(tex.rgb * light, tex.a);
     }
-`
+`;
+
 const mat4 = glMatrix.mat4;
+
+let angle = 0;
+let moveRate = 0.1;
+let turnRate = 0.1;
+let position = [0, 0, -12];
+let cameraDirection = [0, 0, 1];
 
 function startDraw() {
     OBJ.downloadMeshes({
@@ -55,7 +61,7 @@ function startDraw() {
 const Triangle = function (meshes) {
     const canvas = document.getElementById('main-canvas');
     const gl = canvas.getContext('webgl');
-    let canvasColor = [0.2, 0.5, 0.8]
+    let canvasColor = [0.2, 0.5, 0.8];
 
     checkGl(gl);
 
@@ -64,8 +70,6 @@ const Triangle = function (meshes) {
 
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-
-
 
     const vertexShader = gl.createShader(gl.VERTEX_SHADER);
     const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -91,14 +95,9 @@ const Triangle = function (meshes) {
 
     OBJ.initMeshBuffers(gl, meshes.maze);
 
-    // const boxVertBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, meshes.maze.vertexBuffer);
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-
-    // const boxIndicesBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, meshes.maze.indexBuffer);
-    // gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
-    
+
     const posAttribLocation = gl.getAttribLocation(program, 'vertPosition');
     gl.vertexAttribPointer(
         posAttribLocation,
@@ -109,8 +108,6 @@ const Triangle = function (meshes) {
         0
     );
     gl.enableVertexAttribArray(posAttribLocation);
-
-
 
     const textureLocation = gl.getAttribLocation(program, 'textureCoord');
     gl.vertexAttribPointer(
@@ -123,10 +120,7 @@ const Triangle = function (meshes) {
     );
     gl.enableVertexAttribArray(textureLocation);
 
-
-    // const boxTextureBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, meshes.maze.normalBuffer);
-    // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxTextureCoords), gl.STATIC_DRAW);
     
     const normLocation = gl.getAttribLocation(program, 'vertNormal');
     gl.vertexAttribPointer(
@@ -139,15 +133,13 @@ const Triangle = function (meshes) {
     );
     gl.enableVertexAttribArray(normLocation);
 
-
-
     const img = document.getElementById('img');
     const boxTexture = gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D, boxTexture);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texImage2D(
         gl.TEXTURE_2D,
         0,
@@ -155,9 +147,7 @@ const Triangle = function (meshes) {
         gl.RGBA,
         gl.UNSIGNED_BYTE,
         img
-    )
-
-    // render time
+    );
 
     gl.useProgram(program);
 
@@ -166,69 +156,54 @@ const Triangle = function (meshes) {
     const projectionMatLoc = gl.getUniformLocation(program, 'mProjection');
 
     const worldMatrix = mat4.create();
-    const worldMatrix2 = mat4.create();
-
     const viewMatrix = mat4.create();
-    mat4.lookAt(viewMatrix, [0,0,-12], [0,0,0], [0,1,0]);
+    mat4.lookAt(viewMatrix, position, [0, 0, 0], [0, 1, 0]);
+
     const projectionMatrix = mat4.create();
     mat4.perspective(projectionMatrix, glMatrix.glMatrix.toRadian(60), 
-                    canvas.width/canvas.height, 1, 100)
+                    canvas.width / canvas.height, 1, 100);
 
     gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
     gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
     gl.uniformMatrix4fv(projectionMatLoc, gl.FALSE, projectionMatrix);
 
-    let ambientLocation = gl.getUniformLocation(program, 'ambient');
-    let lightDirLoc = gl.getUniformLocation(program, 'lightDirection');
-    let lightColorLoc = gl.getUniformLocation(program, 'lightColor');
+    const ambientLocation = gl.getUniformLocation(program, 'ambient');
+    const lightDirLoc = gl.getUniformLocation(program, 'lightDirection');
+    const lightColorLoc = gl.getUniformLocation(program, 'lightColor');
 
-
-
-    let ambienColor = [0.5, 0.5, 0.2]
-    gl.uniform3f(ambientLocation, ...ambienColor);
+    const ambientColor = [0.5, 0.5, 0.2];
+    gl.uniform3f(ambientLocation, ...ambientColor);
     gl.uniform3f(lightDirLoc, 2.0, 3.0, -1.0);
     gl.uniform3f(lightColorLoc, 0.4, 0.4, 0.2);
-    
-
 
     const identityMat = mat4.create();
-    let rotationMatrix = new Float32Array(16);
-    let translationMatrix = new Float32Array(16);
     let angle = 0;
 
     const loop = function () {
         angle = performance.now() / 1000 / 60 * 23 * Math.PI;
-        mat4.rotate(worldMatrix, identityMat, angle, [1,1,-0.5]);
+        mat4.rotate(worldMatrix, identityMat, angle, [1, 1, -0.5]);
         gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix);
-        
+
+        mat4.lookAt(viewMatrix, position, [position[0] + cameraDirection[0], position[1], position[2] + cameraDirection[2]], [0, 1, 0]);
+        gl.uniformMatrix4fv(viewMatLoc, gl.FALSE, viewMatrix);
+
         gl.clearColor(...canvasColor, 1.0);   // R, G, B,  A 
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
         gl.bindTexture(gl.TEXTURE_2D, boxTexture);
-		gl.activeTexture(gl.TEXTURE0); 
-        // gl.drawArrays(gl.TRIANGLES, 0, 24);
+        gl.activeTexture(gl.TEXTURE0); 
+
         gl.drawElements(gl.TRIANGLES, meshes.maze.indexBuffer.numItems, 
                 gl.UNSIGNED_SHORT, 0); 
 
-        // rotationMatrix = new Float32Array(16);
-        // translationMatrix = new Float32Array(16);
-
-        // mat4.fromRotation(rotationMatrix, angle/2, [1,2,0]);
-        // mat4.fromTranslation(translationMatrix, [2,0,0]);
-        // mat4.mul(worldMatrix2, translationMatrix, rotationMatrix);
-        // gl.uniformMatrix4fv(worldMatLoc, gl.FALSE, worldMatrix2);
-        // gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
-
         requestAnimationFrame(loop);
-    }
+    };
 
     requestAnimationFrame(loop);
-
-} 
-
+}
 
 function checkGl(gl) {
-    if (!gl) {console.log('WebGL not suppoerted, use another browser');}
+    if (!gl) { console.log('WebGL not supported, use another browser'); }
 }
 
 function checkShaderCompile(gl, shader) {
@@ -241,4 +216,45 @@ function checkLink(gl, program) {
     if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
         console.error('linking error', gl.getProgramInfoLog(program));
     }
+}
+
+window.addEventListener(
+    "keydown",
+    (event) => {
+        if (event.defaultPrevented) {
+            return; // Do nothing if event already handled
+        }
+
+        switch (event.code) {
+            case "KeyS":
+            case "ArrowDown":
+                // Handle "back"
+                position[2] += moveRate;
+                break;
+            case "KeyW":
+            case "ArrowUp":
+                // Handle "forward"
+                position[2] -= moveRate;
+                break;
+            case "KeyA":
+            case "ArrowLeft":
+                // Handle "turn left"
+                angle -= turnRate;
+                break;
+            case "KeyD":
+            case "ArrowRight":
+                // Handle "turn right"
+                angle += turnRate;
+                break;
+        }
+
+        updateCameraDirection();
+        event.preventDefault();
+    },
+    true,
+);
+
+function updateCameraDirection() {
+    cameraDirection[0] = Math.sin(angle);
+    cameraDirection[2] = Math.cos(angle);
 }
